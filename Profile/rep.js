1ff7d44d313faa0e79c87861136cd20be2d213be
DarkDev020 <43394215+DarkDev020@users.noreply.github.com>
2020-07-24 12:13:38 -0700

    Update rep.js

--- Profile/rep.js
+++ Profile/rep.js
@@ -1,1 +1,27 @@
-//coming soon!
+const Discord = require('discord.js')
+
+const db = require('quick.db');
+
+
+exports.run = async (bot, message, args) => {
+
+
+let user = message.mentions.member.first() || message.author
+
+
+let rep = await db.fetch(`reputation_${user.id}`)
+
+
+let points = new MessageEmbed()
+
+.setTitle("**Reputation Points**")
+
+.addField("User", user, true)
+
+.addField("Points", rep, true)
+
+.setColor('RANDOM')
+
+message.channel.send(points)
+
+}
